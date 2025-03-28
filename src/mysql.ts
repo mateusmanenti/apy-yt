14b71bf72800d76124e54bbc27d562e922e07629
Mateus Manenti <mateusmanenti98@gmail.com>
2025-03-28 19:06:03 -0300

    yt-projeto

--- src/mysql.ts
+++ src/mysql.ts
@@ -1,11 +1,14 @@
 import mysql from 'mysql';
+import { config } from 'dotenv';
+config();
+
 
 const pool = mysql.createPool({
-    "user":"root",
-    "password": "9898",
-    "database": "api-estoque",
-    "host": "localhost",
-    "port": 3306
-})
+    "user":process.env.USER_DATABASE,
+    "password":process.env.PASSWORD_DATABASE,
+    "database":process.env.DATABASE,
+    "host":process.env.HOST_DATABASE,
+    "port": Number(process.env.PORT_DATABASE)
+});
 
 export {pool};
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,16 @@
 import express from 'express';
-import { categoriaRoutes } from './routes/categoria.routes';
-import { enderecoRoutes } from './routes/endereco.routes';
-import { produtoRoutes } from './routes/produto.routes';
-import { entradaRoutes } from './routes/entrada.routes';
-import { saidaRoutes } from './routes/saida.routes';
-import { estadoEstoque } from './routes/estadoEstoque.routes';
+import { userRoutes } from './routes/user.routes';
+import { videosRoutes } from './routes/videos.routes';
+import { config } from 'dotenv';
 
+config();
 const app = express();
 
-app.use(express.json());
-app.use('/categoria', categoriaRoutes);
-app.use('/endereco', enderecoRoutes);
-app.use('/produto', produtoRoutes);
-app.use('/entrada', entradaRoutes);
-app.use('/saida', saidaRoutes);
-app.use('/estoque', estadoEstoque);
+console.log(process.env.SECRET)
 
-app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
+app.use(express.json());
+app.use('/user', userRoutes);
+app.use('/videos', videosRoutes);
+
+
+app.listen(4000);
